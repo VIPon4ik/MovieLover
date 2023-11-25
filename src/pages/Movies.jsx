70fa13fc9ee7c502c7d47321ab69edf4d5763004
VIPon4ik <umniyd@gmail.com>
2023-11-25 21:32:27 +0200

    Fixed movies query

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getMovies } from 'api/moviesApi';
 import { toast } from 'react-toastify';
-import { useSearchParams, useLocation } from 'react-router-dom';
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom';
 import { MovieList } from './Home.styled';
 import MovieCard from 'components/MovieCard/MovieCard';
 import Loader from 'components/Loader/Loader';
@@ -13,17 +13,21 @@ const Movies = () => {
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const initializateData = async () => {
       if (location.search) {
+        if (location.search.slice(0, 7) !== '?query=') {
+          navigate('/queryError', { replace: true });
+        }
         const query = location.search.slice(7);
         await getMoviesData(query);
       }
     };
 
     initializateData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSubmit = async e => {
@@ -58,7 +62,13 @@ const Movies = () => {
       </Form>
       <MovieList>
         {movies.map(({ id, title, poster_path }) => (
-          <MovieCard location={location} key={id} id={id} title={title} posterPath={poster_path}/>
+          <MovieCard
+            location={location}
+            key={id}
+            id={id}
+            title={title}
+            posterPath={poster_path}
+          />
         ))}
       </MovieList>
     </>
