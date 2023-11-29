2d48b3b58741264b802a0be8631b8117d29b0694
VIPon4ik <umniyd@gmail.com>
2023-11-29 22:36:41 +0200

    Added link to home in PageNotFound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { NotFoundTitle } from './PageNotFound.styled'
+import React from 'react';
+import { NotFoundTitle } from './PageNotFound.styled';
+import { MovieLink } from 'pages/MovieDetails/MovieDetails.styled';
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 
 const PageNotFound = () => {
   return (
+    <>
+      <MovieLink to='/'>
+        <FaLongArrowAltLeft />
+        Home
+      </MovieLink>
       <NotFoundTitle>Page do not exist</NotFoundTitle>
-  )
-}
+    </>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
